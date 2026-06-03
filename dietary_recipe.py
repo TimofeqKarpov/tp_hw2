5580e3aec979106d75bc867559b82e20ea34d6e1
Timofeq Karpov <capybara07@yandex.ru>
2026-06-04 00:14:40 +0300

    Сделан класс Диетический Рецепт

--- dietary_recipe.py
+++ dietary_recipe.py
@@ -2,4 +2,17 @@ from recipe import Recipe
 
 
 class DietaryRecipe(Recipe):
-    pass
+    def __init__(self, title, diet_type, ingredients=[]):
+        super().__init__(title)
+        self.diet_type = diet_type
+        for ingredient in ingredients:
+            self.add_ingredient(ingredient)
+
+    def scale(self, ratio):
+        scale_recipe = super().scale(ratio)
+        new_recipe = DietaryRecipe(self.title, self.diet_type)
+        new_recipe.ingredients = scale_recipe.ingredients
+        return new_recipe
+
+    def __str__(self):
+        return f"{self.diet_type} {super().__str__()}"
