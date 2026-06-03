40d4f986099aadad201608ab9c4ef12c2d26fb33
Timofeq Karpov <capybara07@yandex.ru>
2026-06-03 23:32:15 +0300

    Сделан класс Рецепт.

--- recipe.py
+++ recipe.py
@@ -2,4 +2,29 @@ from ingredient import Ingredient
 
 
 class Recipe:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.ingredients = []
+
+    def add_ingredient(self, ingredient):
+        if ingredient in self.ingredients:
+            self.ingredients[self.ingredients.index(ingredient)].quantity += ingredient.quantity
+        else:
+            self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio):
+        if not Recipe.is_valid_ratio(ratio):
+            raise ValueError("Количество рационов должно быть положительным.")
+        for ingredient in self.ingredients:
+            ingredient.quantity *= ratio
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        ingredients_str = "\n".join(str(i) for i in self.ingredients)
+        return f"Название: {self.title}\nИнгредиенты:\n{ingredients_str}"
